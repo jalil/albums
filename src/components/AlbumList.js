fc1a77cfcab8dfbcb94ec3262fa6da54181ee13e
Jalil <you@example.com>
2017-08-23 04:51:11 -0700

    installed Axios and Albumlist component

---
+++ src/components/AlbumList.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+
+class AlbumList extends Component {
+
+    componentWillMount(){
+       console.log("JALIL"); 
+    }
+    render () {
+    return (
+        <View>
+          <Text> List </Text>
+        </View>
+        );
+    }
+}
+
+
+export default AlbumList;
