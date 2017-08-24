8196945ff51d785293f4140d58c5f64132a39e84
Jalil <you@example.com>
2017-08-23 19:50:14 -0700

    Added more styles for AlbumDetails

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -10,13 +10,16 @@ const AlbumDetail = (props) => {
     return (
         <Card>
         <CardItem>
-        <View>
+        <View style={styles.thumbnailContainerStyle}>
           <Image style={styles.thumbnailsStyle} source={{ uri: props.album.thumbnail_image}}/>
         </View>
          <View style={styles.headerContentStyle}> 
-            <Text>{props.album.title} </Text>
+            <Text style={styles.headerTextStyle} >{props.album.title} </Text>
             <Text>{props.album.artist} </Text>
             </View>
+        </CardItem>
+        <CardItem>
+             <Image style={styles.imageStyle}  source={{uri: props.album.image}} />
         </CardItem>
         </Card>
         );
@@ -29,10 +32,28 @@ const styles = {
         justifyContent: 'space-around'
         },
 
+    headerTextStyle: {
+        fontSize: 18,
+                     },
    thumbnailsStyle: {
      height: 50,
      width: 50,
-      }                    
+      },                  
+
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+         },
+
+    imageStyle: {
+      height: 300,
+      flex: 1,
+      width: null
+
+                }
+
 };
 
 export default AlbumDetail;
