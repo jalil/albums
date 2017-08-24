df3ea96053bf1fbf80203d6177669f8341d69870
Jalil <you@example.com>
2017-08-23 19:12:36 -0700

    Added Image

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View,Image, Text} from 'react-native';
 import Card from './Card';
 import CardItem from './CardItem';
 
@@ -10,11 +10,29 @@ const AlbumDetail = (props) => {
     return (
         <Card>
         <CardItem>
+        <View>
+          <Image style={styles.thumbnailsStyle} source={{ uri: props.album.thumbnail_image}}/>
+        </View>
+         <View style={styles.headerContentStyle}> 
             <Text>{props.album.title} </Text>
+            <Text>{props.album.artist} </Text>
+            </View>
         </CardItem>
         </Card>
         );
 };
 
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+        },
+
+   thumbnailsStyle: {
+     height: 50,
+     width: 50,
+      }                    
+};
 
 export default AlbumDetail;
